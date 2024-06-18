48c2db3c758a45207950b1acedae0bee565aa163
viyog <viyog@eg.dk>
2024-06-18 12:12:45 +0530

    Added class Customer

---
+++ BankManagementSystem/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<Address> CustomerAddress { get; set; }
+        public long PhoneNumber { get; set; }
+        public Account AccountADetails { get; set; }
+        private string Email {  get; set; }
+        private string Password {  get; set; }
+     }
+}
