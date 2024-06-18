57d76d7ebebb67118dd1552a5d045de88ec03e86
viyog <viyog@eg.dk>
2024-06-18 14:21:09 +0530

    Added hashtable CustomerTable i Custoer class

--- BankManagementSystem/Customer.cs
+++ BankManagementSystem/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace BankManagementSystem
         public Account AccountDetails { get; set; }
         private string Email {  get; set; }
         private string Password { get; set; }
+        Hashtable CustomerTable= new Hashtable();
 
         public Customer(string name,string date,List<Address> address,long number,Account details,string email,string password) {
             CustomerName = name;
