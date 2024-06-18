ccdf425d15e56008261358dd2387cc679b88efe4
viyog <viyog@eg.dk>
2024-06-18 12:26:34 +0530

    Added class Account

--- BankManagementSystem/Customer.cs
+++ BankManagementSystem/Customer.cs
@@ -12,8 +12,19 @@ namespace BankManagementSystem
         public DateTime DateOfBirth { get; set; }
         public List<Address> CustomerAddress { get; set; }
         public long PhoneNumber { get; set; }
-        public Account AccountADetails { get; set; }
+        public Account AccountDetails { get; set; }
         private string Email {  get; set; }
-        private string Password {  get; set; }
+        private string Password { get; set; }
+
+        public Customer(string name,string date,List<Address> address,long number,Account details,string email,string password) {
+            CustomerName = name;
+            DateOfBirth= Convert.ToDateTime(date);
+            CustomerAddress = address;
+            PhoneNumber = number;
+            AccountDetails = details;
+            Email = email;
+            Password = password;
+        
+        }
      }
 }
