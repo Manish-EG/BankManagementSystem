355963dd875c29c3b85ba8b1933e34e149f8b0d6
rchan <rchan@eg.dk>
2024-06-18 13:57:08 +0530

    bank and bankbranch class created

---
+++ BankManagementSystem/BankBranch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagementSystem
+{
+    public  class BankBranch
+    {
+        public string BranchName;
+
+     
+        public Hashtable IFSCCodeList = new Hashtable()
+        {
+            {"Yeyyadi",new List<String>{ "EGIND0005670", "wrkwrk,Yeyyadi","577101","Manglore","India" } },
+            {"Kapikad",new List<String>{ "EGIND0005780","AjanthaBusiness","577101","Manglore","India" } }
+        };
+        public readonly string IFSCCode;
+        Address branchAddress;
+        public BankBranch(string branchName)
+        {
+            BranchName = branchName;
+            List<string> retrievedList = IFSCCodeList[branchName] as List<string>;
+            IFSCCode = retrievedList?[0];
+            branchAddress=new Address(retrievedList?[1], retrievedList[2], retrievedList?[3], retrievedList?[4]);
+            
+        }
+        
+
+
+        }
+    }
+}
