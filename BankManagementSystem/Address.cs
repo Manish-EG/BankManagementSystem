7e771c16a7ff0d651d8a4a6c10ee197de3ac2854
manish-eg <makul@eg.dk>
2024-06-18 12:55:49 +0530

    BaseEntity and Address class are created

---
+++ BankManagementSystem/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace BankManagementSystem
+{
+    public class Address
+    {
+        public string LocationAddress { get; set; }
+        public int PinCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public Address(string location,int pincode,string city,string country) {
+            LocationAddress = location;
+            PinCode = pincode;
+            City = city;
+            Country = country;
+        }
+    }
+}
