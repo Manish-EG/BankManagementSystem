27b969d8129b4d0439d7c9910a1b2ca3ef9f7e0a
rchan <rchan@eg.dk>
2024-06-19 10:12:59 +0530

    succesfully pulled the folder structure changes

--- BankManagementSystem/Model/Address.cs
+++ BankManagementSystem/Model/Address.cs
@@ -1,13 +1,14 @@
-﻿namespace BankManagementSystem
+﻿namespace BankManagementSystem.Model
 {
-    public class Address
+    public class AddressModel
     {
         public string LocationAddress { get; set; }
         public int PinCode { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
 
-        public Address(string location,int pincode,string city,string country) {
+        public AddressModel(string location, int pincode, string city, string country)
+        {
             LocationAddress = location;
             PinCode = pincode;
             City = city;
